e40f7270f968155ca965b374b0eef6a4ab70dbca
Logan Mohseni <mohsenil85@gmail.com>
2014-01-27 09:37:22 -0800

    stupid link up script

--- config-init.sh
+++ config-init.sh
@@ -2,6 +2,11 @@
 
 #link all my shit up
 
-for i in `ls`
-ln -s $i ~/.$i
+for i in `ls dotfiles`; do
+	if [ -h ~/.$i ] 
+	then
+		echo $i "exists"
+		rm ~/.$i
+	fi
+	ln -s ~/configs/dotfiles/$i ~/.$i
 done
